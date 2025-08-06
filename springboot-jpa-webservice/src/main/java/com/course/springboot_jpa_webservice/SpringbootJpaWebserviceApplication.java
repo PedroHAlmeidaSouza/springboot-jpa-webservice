9e37877c13f0ccc4ea4e8040c89535536ebccba6
PedroHAlmeidaSouza <ph8584774@gmail.com>
2025-08-06 17:43:19 -0300

    Criação do projeto
    Signed-off-by: PedroHAlmeidaSouza <ph8584774@gmail.com>

---
+++ springboot-jpa-webservice/src/main/java/com/course/springboot_jpa_webservice/SpringbootJpaWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.course.springboot_jpa_webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJpaWebserviceApplication.class, args);
+	}
+
+}
---
+++ springboot-jpa-webservice/src/test/java/com/course/springboot_jpa_webservice/SpringbootJpaWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.springboot_jpa_webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJpaWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
