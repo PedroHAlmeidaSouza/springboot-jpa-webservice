972712b96a7fad4cc487a89dd2cbabe9ff9aa9b6
Pedro Henrique <ph8584774@gmail.com>
2025-08-16 22:21:37 -0300

    Banco de dados H2, test profile, JPA

--- springboot-jpa-webservice/src/main/java/com/course/springboot_jpa_webservice/entities/User.java
+++ springboot-jpa-webservice/src/main/java/com/course/springboot_jpa_webservice/entities/User.java
@@ -1,9 +1,15 @@
 package com.course.springboot_jpa_webservice.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
