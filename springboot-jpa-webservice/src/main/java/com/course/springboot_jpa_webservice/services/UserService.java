230cf3533b99e5cb37bec2e0833cdfd07cbac1c4
Pedro Henrique <ph8584774@gmail.com>
2025-08-20 22:35:46 -0300

    Tratamento de exceções na entidade User

--- springboot-jpa-webservice/src/main/java/com/course/springboot_jpa_webservice/services/UserService.java
+++ springboot-jpa-webservice/src/main/java/com/course/springboot_jpa_webservice/services/UserService.java
@@ -2,6 +2,11 @@ package com.course.springboot_jpa_webservice.services;
 
 import com.course.springboot_jpa_webservice.entities.User;
 import com.course.springboot_jpa_webservice.repositories.UserRepository;
+import com.course.springboot_jpa_webservice.services.exceptions.DatabaseException;
+import com.course.springboot_jpa_webservice.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,7 +27,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User save(User obj) {
@@ -30,13 +35,26 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        userRepository.deleteById(id);
+        try {
+            if (!userRepository.existsById(id)) {
+                throw new ResourceNotFoundException(id);
+            }
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
-        User entity = userRepository.getReferenceById(id);
-        updateData(entity, obj);
-        return userRepository.save(entity);
+        try {
+            User entity = userRepository.getReferenceById(id);
+            updateData(entity, obj);
+            return userRepository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
---
+++ springboot-jpa-webservice/src/main/java/com/course/springboot_jpa_webservice/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,13 @@
+package com.course.springboot_jpa_webservice.services.exceptions;
+
+import java.io.Serial;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id: " + id);
+    }
+}
