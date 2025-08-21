85865780a1678f2a0427742de46de86faad4d957
Pedro Henrique <ph8584774@gmail.com>
2025-08-20 21:44:49 -0300

    CRUD User

--- springboot-jpa-webservice/src/main/java/com/course/springboot_jpa_webservice/controllers/UserController.java
+++ springboot-jpa-webservice/src/main/java/com/course/springboot_jpa_webservice/controllers/UserController.java
@@ -3,11 +3,10 @@ package com.course.springboot_jpa_webservice.controllers;
 import com.course.springboot_jpa_webservice.entities.User;
 import com.course.springboot_jpa_webservice.services.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -27,10 +26,29 @@ public class UserController {
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping(value = "/{userId}")
-    public ResponseEntity<User> findById(@PathVariable Long userId) {
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
 
-        User obj = userService.findById(userId);
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> save(@RequestBody User obj) {
+        obj = userService.save(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = userService.update(id, obj);
         return ResponseEntity.ok().body(obj);
     }
 }
